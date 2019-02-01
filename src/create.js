e310096a31626f35b458235b94b7f1e6a87260fc
Matt Travi <programmer@travi.org>
2019-01-31 20:56:15 -0600

    fix(create): returned the promise from the repo creation
    for #1

--- src/create.js
+++ src/create.js
@@ -1,5 +1,5 @@
 import {factory} from './github-client-factory';
 
 export default function (name, visibility) {
-  factory().repos.createForAuthenticatedUser({name, private: 'Private' === visibility});
+  return factory().repos.createForAuthenticatedUser({name, private: 'Private' === visibility});
 }

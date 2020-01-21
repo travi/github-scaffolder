3937a0d420205602a0614d110563bb0c607584d5
Matt Travi <programmer@travi.org>
2020-01-20 22:32:46 -0600

    feat(decisions): enabled providing decisions for the prompts

--- src/prompt.js
+++ src/prompt.js
@@ -1,12 +1,12 @@
 import gitConfig from 'git-config';
-import {prompt as promptWithInquirer} from 'inquirer';
+import {prompt as promptWithInquirer} from '@form8ion/overridable-prompts';
 
-export function prompt({account} = {}) {
+export function prompt({account, decisions} = {}) {
   return promptWithInquirer([
     {
       name: 'repoOwner',
       message: 'What is the id of the repository owner?',
       default: account || (gitConfig.sync().github ? gitConfig.sync().github.user : '')
     }
-  ]);
+  ], decisions);
 }
--- test/unit/prompt-test.js
+++ test/unit/prompt-test.js
@@ -1,5 +1,5 @@
 import gitConfig from 'git-config';
-import inquirer from 'inquirer';
+import * as prompts from '@form8ion/overridable-prompts';
 import {assert} from 'chai';
 import sinon from 'sinon';
 import any from '@travi/any';
@@ -9,11 +9,12 @@ suite('prompt', () => {
   let sandbox;
   const githubUser = any.word();
   const answers = any.listOf(any.string);
+  const decisions = any.simpleObject();
 
   setup(() => {
     sandbox = sinon.createSandbox();
 
-    sandbox.stub(inquirer, 'prompt');
+    sandbox.stub(prompts, 'prompt');
     sandbox.stub(gitConfig, 'sync');
   });
 
@@ -21,39 +22,39 @@ suite('prompt', () => {
 
   test('that the options are optional', async () => {
     gitConfig.sync.returns({github: {user: githubUser}});
-    inquirer.prompt.resolves(answers);
+    prompts.prompt.resolves(answers);
 
     assert.equal(await prompt(), answers);
   });
 
   test('that the github user is provided as the default owner value if available in the global config', async () => {
     gitConfig.sync.returns({github: {user: githubUser}});
-    inquirer.prompt
+    prompts.prompt
       .withArgs([
         {
           name: 'repoOwner',
           message: 'What is the id of the repository owner?',
           default: githubUser
         }
-      ])
+      ], decisions)
       .resolves(answers);
 
-    assert.equal(await prompt({}), answers);
+    assert.equal(await prompt({decisions}), answers);
   });
 
   test('that the github user is not used as the default owner value an override is provided', async () => {
     const account = any.word();
     gitConfig.sync.returns({github: {user: githubUser}});
-    inquirer.prompt
+    prompts.prompt
       .withArgs([
         {
           name: 'repoOwner',
           message: 'What is the id of the repository owner?',
           default: account
         }
-      ])
+      ], decisions)
       .resolves(answers);
 
-    assert.equal(await prompt({account}), answers);
+    assert.equal(await prompt({account, decisions}), answers);
   });
 });

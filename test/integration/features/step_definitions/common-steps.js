3111a7cd365399684f65bae073fc4a124abae704
Matt Travi <programmer@travi.org>
2020-02-09 00:14:06 -0600

    fix(dependencies): upgraded to the v17 beta of octokit
    for octokit/rest.js#1546

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -1,5 +1,5 @@
 import {resolve} from 'path';
-import {promises, promises as fsPromises} from 'fs';
+import {promises as fsPromises} from 'fs';
 import {After, Before, When} from 'cucumber';
 import stubbedFs from 'mock-fs';
 import any from '@travi/any';
@@ -8,18 +8,6 @@ import {scaffold} from '../../../../src';
 const {readFile} = fsPromises;
 const pathToNodeModules = [__dirname, '../../../../', 'node_modules/'];
 const debug = require('debug')('test');
-
-function loadOctokitFiles(octokitFiles) {
-  return octokitFiles
-    .map(file => promises.readFile(resolve(...pathToNodeModules, 'octokit-pagination-methods/lib/', file)));
-}
-
-function buildOctokitFileMap(octokitFiles) {
-  return (acc, fileContents, index) => ({
-    ...acc,
-    [octokitFiles[index]]: fileContents
-  });
-}
 
 Before(function () {
   this.projectName = any.word();
@@ -33,14 +21,10 @@ After(function () {
 });
 
 When('the project is scaffolded', async function () {
-  const octokitFiles = await promises.readdir(resolve(...pathToNodeModules, 'octokit-pagination-methods/lib/'));
   stubbedFs({
     ...this.netrcContent && {[`${process.env.HOME}/.netrc`]: this.netrcContent},
     [`${process.env.HOME}/.gitconfig`]: `[github]\n\tuser = ${this.githubUser}`,
     node_modules: {
-      'octokit-pagination-methods': {
-        lib: (await Promise.all(loadOctokitFiles(octokitFiles))).reduce(buildOctokitFileMap(octokitFiles), {})
-      },
       '@travi': {
         'cli-messages': {
           node_modules: {
